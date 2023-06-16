057d29ac7beb0b3f4c8df0c52bfed6ec2a81c911
vpm789 <vpm789@gmail.com>
2023-06-16 17:36:15 +0300

    mocks_init

---
+++ src/test/java/pro/sky/java/course2/service/EmployeesServiceTest.java
@@ -0,0 +1,8 @@
+package pro.sky.java.course2.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+public class EmployeesServiceTest {
+}
